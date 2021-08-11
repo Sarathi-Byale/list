aad93ffec56ac977f54a244aa7a5653a05542170
Sarathi <54929256+Sarathi-Byale@users.noreply.github.com>
2021-08-11 09:29:11 +0530

    Update list.append.py

--- list.append.py
+++ list.append.py
@@ -1,11 +1,15 @@
 a=[]
-n=int(input())
+print("Enter the length of list")
+n=int(input()) #How much Elements you need in list
 for i in range (n):
-  c=int(input())
+  print("Enter the elements you need in list:")
+  c=int(input()) # Number You have to append in list
   a.append(c)
+print("Your list is:")
 print(a)
 n=[]
 for z in range(len(a)):
   if a[z]>0:
     n.append(a[z])
+print("Your list after removing the negative numbers")
 print(n)
